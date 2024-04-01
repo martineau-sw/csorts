cbb14313543a5b2662e2b6992699a92a811d9333
Seth Martineau <martineausw@gmail.com>
2024-04-01 00:44:38 -0400

    Implement merge sort correctly

--- src/merge-sort.c
+++ src/merge-sort.c
@@ -2,58 +2,85 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-int print_array(size_t c, double array[c]);
+int print_array(char label, size_t size, double array[size]);
 void split_array(size_t start, size_t end, double a[end-start]);
 
 int main(void) {
     size_t const size = 4;
     double a[size] = {
-        [0] = 3,
-        [1] = 1,
-        [2] = 2,
-        [3] = 4
+        [0] = 8,
+        [1] = 7,
+        [2] = 6,
+        [3] = 5,
+//        [4] = 4,
+//        [5] = 3,
+ //       [6] = 2,
+  //      [7] = 1,
     };
-    print_array(size, a);
-    split_array(0, size, a);
-    print_array(size, a);
+    split_array(0, size-1, a);
+    
+    printf("\nfinal:");
+    print_array('a', size, a);
     return EXIT_SUCCESS;
 }
 
-int print_array(size_t c, double array[c]) {
-    for (size_t i = 0; i < c; ++i) {
-        printf("element %zu contains value %f\n",
+int print_array(char label, size_t size, double array[size]) {
+    printf("\nsize:%zu\n", size);
+    for (size_t i = 0; i < size; ++i) {
+        printf("%c[%zu] = %f\n",
+                label,
                 i,
                 array[i]);
     }
-    
-    printf("\n");
 
     return EXIT_SUCCESS;
 }
 
 void split_array(size_t start, size_t end, double array[end-start]) { 
-    printf("splitting array\tstart: %zu\tend: %zu\t", start, end);
 
-    size_t midpoint = (start + end) / 2;
+    size_t mid = (start + end) / 2;
     
-    printf("midpoint: %zu\n", midpoint);
 
-    if(start == midpoint) {
+    if(start == end) {
         return;
     }
 
-    split_array(start, midpoint, array);
-    split_array(midpoint+1, end, array);
-     
-    for (size_t i = start; i < end; i += 2) {
-        if (array[(start+i)/2] <= array[(midpoint+i)/2]) {
-            array[i] = array[start+i/2];
-            array[i+1] = array[(midpoint+i)/2];
+    split_array(start, mid, array);
+    split_array(mid+1, end, array);
+    
+    size_t l_size = (mid-start)+1;
+    size_t r_size = (end-mid);
+
+    double l[l_size];
+    double r[r_size];
+
+    for (size_t i = 0; i < l_size; i++) {
+        l[i] = array[start+i];
+    }
+    for (size_t i = 0; i < r_size; i++) {
+        r[i] = array[mid+1+i];
+    }
+
+    print_array('l', l_size, l);
+    print_array('r', r_size, r);
+
+    size_t l_i = 0;
+    size_t r_i = 0;
+    size_t i = start;
+    while (l_i < l_size && r_i < r_size) {
+        if (l[l_i] <= r[r_i]) {
+            array[i++] = l[l_i++];
         } else {
-            array[i] = array[(midpoint+i)/2];
-            array[i+1] = array[(start+i)/2];
+            array[i++] = r[r_i++];
         }
     }
-    
+
+    while (l_i < l_size) {
+        array[i++] = l[l_i++];
+    }
+
+    while (r_i < r_size) {
+        array[i++] = r[r_i++];
+    }
 }
 
